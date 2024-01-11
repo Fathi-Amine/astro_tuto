549ff10710f8803c517e2900cd036d5c3575ffe6
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2024-01-11 15:34:08 +0100

    setting up and installing astro and dependencies to learn astro by building a blog

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,md,mdx,}'],
+  theme: {
+    extend: {},
+  },
+  integrations: [tailwind()]
+});
