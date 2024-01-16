63f886ee75ccd573ab7a45f108143c0997e82e26
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2024-01-16 15:22:45 +0100

    learning about constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'TechPeople Blog'
+export const SITE_DESCRIPTION = 'Articles, Stories & Tutorials for Tech People'
+export const SITE_HOMEPAGE_LIMIT = 7
+export const ARTICLES_PER_PAGE = 6
